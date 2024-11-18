d0e76005bb700a7573c3f6c966a65e9ef473c512
unknown <tifuhvirginie@yahoo.com>
2024-11-17 21:17:35 -0500

    added inline styling

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import Content from './components/Content';
 import Footer from './components/Footer';  
 
 function App() {
+  const appContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    height: '100vh', 
+    textAlign: 'center', 
+  };
+
   return (
-    <>
+    <div style={appContainerStyle}>
+    
       <Header />
       <Content color="blue" text="This is my first React Application!" />
       <Content color="red" text="Wish me luck..." />
       <Content color="green" text="I think I've got it!" />
-      <Footer />    </>
+      <Footer />  
+    </div>
   );
 }
+
 
 export default App;
 
